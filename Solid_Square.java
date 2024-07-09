a5ec8884cfae5b9043e8d9daeae005eabb2175f3
5id5hah <157269117+5id5hah@users.noreply.github.com>
2024-07-09 19:21:06 +0530

    Add files via upload

---
+++ Solid_Square.java
@@ -0,0 +1,18 @@
+import java.util.regex.Pattern;
+
+public class Solid_Square {
+        public static void main(String[] args) {
+            Pattern(5);
+        }
+           public static void Pattern(int n){
+            for (int i = 1; i < n; i++) {
+                for (int j = 1; j < n; j++) {
+                    System.out.print(" * ");
+                }
+                System.out.println(" ");
+
+            }
+        }
+    }
+
+
