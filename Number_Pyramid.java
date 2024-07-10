822731b093655b3670019cc7949df517e5d9abe0
5id5hah <157269117+5id5hah@users.noreply.github.com>
2024-07-10 22:27:11 +0530

    Add files via upload

---
+++ Number_Pyramid.java
@@ -0,0 +1,23 @@
+public class Number_Pyramid {
+    public static void main(String[] args) {
+        Pattern(5);
+    }
+    static void Pattern(int n){
+        for (int row = 1; row <= n ; row++) {
+
+            for (int spaces = 0; spaces < n - row; spaces++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+
+            }
+            for (int col = 2; col <= row ; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println(" ");
+
+        }
+
+    }
+}
